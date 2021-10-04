411548529dc298132222edaef70bea90603e7a21
deepak5141 <70088686+deepak5141@users.noreply.github.com>
2021-10-04 20:52:18 +0530

    Update transactions.js

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getTransactions, addTransaction, deleteTransaction } = require('../controllers/transactions');
 
-router
-  .route('/')
-  .get(getTransactions)
-  .post(addTransaction);
 
 
 
