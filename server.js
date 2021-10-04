716cb0f751354a4e7b40f280c2db00d17a4db7ff
deepak5141 <70088686+deepak5141@users.noreply.github.com>
2021-10-04 19:07:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use('/api/v1/transactions', transactions);
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
