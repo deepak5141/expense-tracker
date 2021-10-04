c41db44a85f1c4a48316a2ed8cd5c21289232372
deepak5141 <70088686+deepak5141@users.noreply.github.com>
2021-10-04 19:06:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
-const morgan = require('morgan');
-const connectDB = require('./config/db');
+
 
 dotenv.config({ path: './config/config.env' });
 
